dc3d7123639a1b0d4c0c1431ba95c3135cf099bb
Sima Nerush <2002ssn@gmail.com>
2022-01-07 22:01:49 -0800

    Set up MVVM

---
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,70 @@
+//
+//  EmojiArtDocumentView.swift
+//  EmojiArt
+//
+//  Created by Serafima Nerush on 1/7/22.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            documentBody
+            palette
+        }
+    }
+    
+    var documentBody: some View {
+        Color.yellow
+    }
+    
+    var palette: some View {
+        ScrollingEmojisView(emojis: testEmojis)
+    }
+    
+    let testEmojis = "🎉🚕🥂🪴🚞📲🖥🙃🎛📟💛💽☎️⏰🔌🧯🌃🌉🌁"
+}
+
+struct ScrollingEmojisView: View {
+    let emojis: String
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis.map { String($0) }, id: \.self) { emoji in
+                    Text(emoji)
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct EmojiArtDocumentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiArtDocumentView(document: EmojiArtDocument())
+    }
+}
