3bd440b4b99056bf3651213b0608ee456757f9c0
Sima Nerush <2002ssn@gmail.com>
2022-01-12 18:08:26 -0800

    Add functionality to Palette Manager

--- PaletteManager.swift
+++ PaletteManager.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct PaletteManager: View {
     @EnvironmentObject var store: PaletteStore
+    @Environment(\.presentationMode) var presentationMode
+    
     @Environment(\.colorScheme) var colorScheme
+    
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
@@ -20,18 +24,45 @@ struct PaletteManager: View {
                             Text(palette.name)
                             Text(palette.emojis)
                         }
+                        .gesture(editMode == .active ? tap : nil)
                     }
+                }
+                .onDelete { indexSet in
+                    store.palettes.remove(atOffsets: indexSet)
+                }
+                .onMove { indexSet, newOffset in
+                    store.palettes.move(fromOffsets: indexSet, toOffset: newOffset)
                 }
             }
             .navigationTitle("Manage Palettes")
             .navigationBarTitleDisplayMode(.inline)
+            // Both the list and the edit button are looking at the same editMode, because it's a local State var. editMode is changed by EditButton() that SwiftUI provides
+            .toolbar {
+                ToolbarItem {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    if presentationMode.wrappedValue.isPresented {
+                        Button("Close") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+               
+            }
+            .environment(\.editMode, $editMode)
         }
+    }
+    
+    var tap: some Gesture {
+        TapGesture().onEnded { }
     }
 }
 
 struct PaletteManager_Previews: PreviewProvider {
     static var previews: some View {
         PaletteManager()
+            .previewDevice("iPhone 8")
             .environmentObject(PaletteStore(named: "Preview"))
     }
 }
